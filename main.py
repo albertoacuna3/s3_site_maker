fbf4f6c2c44b2a4c39d63b6e4a3abe9fe1c417e6
albertoacuna3 <root@ip-172-31-27-107.us-west-2.compute.internal>
2018-08-08 23:09:24 +0000

    Added Cement command line execution

--- main.py
+++ main.py
@@ -4,67 +4,87 @@ import json
 from os import listdir, getcwd
 from os.path import isfile, join
 
-#TODO:Have to double check how I want to do credentials
-s3 = boto3.resource('s3')
-s3_bucket = None
-config_file = None
+from cement.core.foundation import CementApp
+from cement.core import hook
+from cement.utils.misc import init_defaults
 
-def load_s3_bucket_object():
-    global s3_bucket
+class AWSController:
+    def __init__(self, location):
+        self.location = location
+        self.s3 = boto3.resource('s3')
+        self.config_file = self.load_config_file()
+        self.s3_bucket = self.load_s3_bucket_object()
 
-    if config_file is None:
-        load_config_file()
+    def load_s3_bucket_object(self):
+        if self.config_file is None:
+            self.load_config_file()
 
-    s3_bucket = s3.Bucket(config_file['BucketName'])
+        self.s3_bucket = self.s3.Bucket(self.config_file['BucketName'])
 
-def put_directory_in_bucket():
-    global s3_bucket
+    def put_directory_in_bucket(self, location):
+        if self.s3_bucket is None:
+            self.load_s3_bucket_object()
 
-    if s3_bucket is None:
-        load_s3_bucket_object()
+        files_only = [f for f in listdir(location) if isfile(join(location, f))]
+        for fn in files_only:
+            filename, file_extension = os.path.splitext(fn)
+            self.s3_bucket.upload_file(join(location, fn), fn, ExtraArgs={'ACL':'public-read', 'ContentType': self.get_file_content_type(file_extension[1:])})
 
-    current_path = getcwd()
-    files_only = [f for f in listdir(current_path) if isfile(join(current_path, f))]
+    def get_file_content_type(self, filename_ext):
+        mimetypes = {
+        'html': 'text/html',
+        'json': 'application/json',
+        'css': 'text/css',
+        'other': 'text/plain'
+        }
 
-    for fn in files_only:
-        filename, file_extension = os.path.splitext(fn)
-        s3_bucket.upload_file(fn, fn, ExtraArgs={'ACL':'public-read', 'ContentType':get_file_content_type(file_extension[1:])})
+        return mimetypes.get(filename_ext, mimetypes.get('other'))
 
-def get_file_content_type(filename_ext):
-    mimetypes = {
-       'html': 'text/html',
-       'json': 'application/json',
-       'css': 'text/css',
-       'other': 'text/plain'
-    }
+    def get_bucket_objects(self, bucket):
+        response =  self.s3.meta.client.list_objects(
+            Bucket=bucket
+        )
+        contents = response['Contents']
+        formatted_contents = [{'Key': d['Key'], 'Size': d['Size'], 'LastModified': d['LastModified'].strftime("%B %d, %Y")} for d in contents]
+        return formatted_contents
 
-    return mimetypes.get(filename_ext, mimetypes.get('other'))
+    def get_current_directory(self):
+        return os.getcwd()
 
-def get_bucket_objects(bucket):
-    response =  s3.meta.client.list_objects(
-        Bucket=bucket
-    )
-    contents =response['Contents']
-    formatted_contents = [{'Key': d['Key'], 'Size': d['Size'], 'LastModified': d['LastModified'].strftime("%B %d, %Y")} for d in contents]
-    return formatted_contents
+    def push_files_to_s3(self, filename, bucket, filename_key):
+        #TODO: Load files to S3
+        self.s3.meta.client.upload_file(filename, bucket, filename_key)
 
-def get_current_directory():
-    return os.getcwd()
-
-def push_files_to_s3(filename, bucket, filename_key):
-    #TODO: Load files to S3
-    s3.meta.client.upload_file(filename, bucket, filename_key)
-
-def load_config_file():
-    #TODO: Read a config file. Currently only check current directory
-    global config_file
-    with open('aws_site_maker.json') as f:
-        config_file = json.load(f)
+    def load_config_file(self):
+        config_file = join(self.location, 'aws_site_maker.json')
+        with open(config_file) as f:
+            self.config_file = json.load(f)
 
 
-load_config_file()
 
-put_directory_in_bucket()
-#contents = get_bucket_objects(config_file['BucketName'])
+# define our default configuration options
+defaults = init_defaults('myapp')
+defaults['myapp']['debug'] = False
+defaults['myapp']['some_param'] = 'some value'
 
-#print(contents)
+# define any hook functions here
+def my_cleanup_hook(app):
+    pass
+
+class MyApp(CementApp):
+    class Meta:
+        label = 'myapp'
+        config_defaults = defaults
+        hooks = [
+            ('pre_close', my_cleanup_hook),
+        ]
+
+with MyApp() as app:
+    app.args.add_argument('-l', '--location', action='store', metavar='STR', help='the directory you want to run this on')
+
+    app.run()
+
+    if app.pargs.location:
+        location = app.pargs.location
+        aws = AWSController(location)
+        aws.put_directory_in_bucket(location)
